91f0a08fe485880100b7b58388b1c525aea6ce4a
kyb4188 <kyb43334188@gmail.com>
2024-11-05 17:33:54 +0900

    feat main.py

--- gui/main/main.py
+++ gui/main/main.py
@@ -1,5 +1,7 @@
+# pip install PyQt5==5.14.2
+
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -13,7 +15,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # 세 번째 페이지 UI 파일 로드
         self.page3 = uic.loadUi("./payment_information.ui")
-        
+
+        # 네 번째 페이지 UI 파일 로드
+        self.page4 = uic.loadUi("./wait_page.ui")
+                
         
         # 스택 위젯 생성
         self.stacked_widget = QtWidgets.QStackedWidget(self)
@@ -23,12 +28,25 @@ class MainWindow(QtWidgets.QMainWindow):
         self.stacked_widget.addWidget(self.page1)
         self.stacked_widget.addWidget(self.page2)
         self.stacked_widget.addWidget(self.page3)
+        self.stacked_widget.addWidget(self.page4)
+
+        # 이미지 추가
+        self.add_image_to_label()
+
+
         
         # 버튼 클릭 시 페이지 전환 연결
         self.page1.select_ice_cream.clicked.connect(self.show_page2)
         self.page2.back_button.clicked.connect(self.show_page1)
         self.page2.select_complete.clicked.connect(self.show_page3)
         self.page3.back_button.clicked.connect(self.show_page2)
+
+
+        # 페이지3에서 5초 후에 페이지4로 이동하기 위한 타이머 설정
+        self.timer = QtCore.QTimer(self)
+        self.timer.setSingleShot(True)  # 단 한번만 실행
+        self.timer.timeout.connect(self.show_page4)
+
 
         # 각 그룹의 버튼을 설정
         self.ice_cream_buttons = [self.page2.chocolate_ice_cream, self.page2.strawberry_ice_cream, self.page2.vanilla_ice_cream]
@@ -42,6 +60,24 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # 창을 최대화된 상태로 표시
         self.showMaximized()
+
+    def add_image_to_label(self):
+        # QGraphicsScene 생성
+        scene = QtWidgets.QGraphicsScene(self)
+        
+        # QPixmap 생성
+        pixmap = QtGui.QPixmap("./image/image.png")  # 이미지 파일 경로
+        if not pixmap.isNull():  # 이미지가 유효한지 확인
+            # QPixmap을 QGraphicsPixmapItem으로 변환하여 scene에 추가
+            scene.addPixmap(pixmap)
+            # QGraphicsView에 Scene 설정
+            self.page4.graphicsView.setScene(scene)
+            self.page4.graphicsView.setRenderHint(QtGui.QPainter.Antialiasing)
+            self.page4.graphicsView.setRenderHint(QtGui.QPainter.SmoothPixmapTransform)
+        else:
+            print("이미지를 로드할 수 없습니다.")
+
+
 
     def setup_button_group(self, buttons, selected_color):
         # 그룹의 모든 버튼 초기 색상 설정 및 클릭 시 선택 상태 설정
@@ -58,6 +94,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def show_page3(self):
         self.stacked_widget.setCurrentWidget(self.page3)
+        self.timer.start(1000)  # 5초 후에 show_page4 호출
+    
+    def show_page4(self):
+        self.stacked_widget.setCurrentWidget(self.page4)
 
     def select_button(self, button_group, selected_button, color):
         # 선택된 그룹의 모든 버튼을 기본 색상으로 리셋
